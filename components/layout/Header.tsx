8fb73189e4f6dfd3b9562b43429f98262d637d69
rurigrass <ruripg@gmail.com>
2023-06-07 16:46:50 +0200

    added nav and icons

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -9,16 +9,35 @@ import LogoutButton from "./LogoutButton";
 
 const Header = () => {
   // const router = useRouter();
-  const { data: session } = useSession();  
+  const { data: session } = useSession();
+
+  const items = [
+    { label: "Notifications", href: "/notifications", icon: "", auth: true },
+    { label: "Profile", href: "/users/123", icon: "", auth: true },
+    { label: "yep", href: "", icon: "", auth: true },
+  ];
 
   return (
-    <div className="h-16 bg-black_ct flex items-center justify-between">
-      <Logo />
-      <div className="mr-2">
-        {/* <Button small label="Post" onClick={() => router.push("/")} /> */}
-        {session ? <LogoutButton /> : <LoginButton />}
+    <nav className="h-16 w-full bg-black_ct ">
+      <div className="flex items-center justify-between h-full w-full px-4 2xl:px-16">
+        <div>
+          <Logo />
+        </div>
+        <div>
+          <ul className="hidden sm:flex items-center justify-center">
+            {items.map((item) => (
+              <li className="ml-10 uppercase hover:border-b hover:cursor-pointer text-xl ">
+                {item.label}
+              </li>
+            ))}
+            <li className="ml-10 uppercase text-xl">
+              {session ? <LogoutButton /> : <LoginButton />}
+            </li>
+          </ul>
+          <div></div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
