2a487c5d797773a9d805ac814b20978c73cddd11
rurigrass <ruripg@gmail.com>
2023-06-09 20:22:20 +0200

    sidebar hood false default

--- hooks/useSidebar.ts
+++ hooks/useSidebar.ts
@@ -7,7 +7,7 @@ interface ISidebarStore {
 }
 
 const useSidebar = create<ISidebarStore>((set) => ({
-  isOpen: true,
+  isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }));
