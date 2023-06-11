1fd245b5644fbeee36e9113b8a086b849477672f
rurigrass <ruripg@gmail.com>
2023-06-11 17:49:46 +0200

    usersroutes

---
+++ app/api/users/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/libs/prismadb";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    const users = await prisma.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+};
+
+export default handler;
